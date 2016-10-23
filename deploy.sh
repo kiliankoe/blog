6ad6cc575075f52502bbe606357ee54016073f17
Kilian Koeltzsch <me@kilian.io>
2016-10-23 03:16:54 +0200

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying current version to gh-pages...\033[0m"
+
+hugo
+
+git add public/*
+
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -S -m "$msg"
+
+git subtree push --prefix=public git@github.com:kiliankoe/blog.git gh-pages
